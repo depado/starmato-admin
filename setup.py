c94ac7b2739c487b5a874678e219a8d508654ee4
matm <mathias@monnerville.com>
2014-09-28 12:37:22 +0200

    Make starmato a Python package

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='starmato-admin',
-    namespace_packages = ['starmato'],
     version='0.1',
     packages=['starmato', 'starmato.admin'],
     include_package_data=True,
