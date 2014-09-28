5006ca0b66734e94dee71f9f5568e6dec9de8b36
matm <mathias@monnerville.com>
2014-09-28 13:07:23 +0200

    setup: packaged with media and locale directories

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-print find_packages()
-
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 # allow setup.py to be run from any path
@@ -11,9 +9,11 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='starmato-admin',
     version='0.1',
-    packages=['starmato', 'starmato.admin'],
-    include_package_data=True,
-    license='BSD License',  # example license
+    packages=find_packages(),
+    package_data={
+        'starmato.admin': ['media/admin/js/*', 'media/admin/css/*', 'media/admin/images/*', 'locale/fr_FR/LC_MESSAGES/*'],
+    },
+    license='MIT',
     description='A Django app to upgrade django admin.',
     long_description=README,
     url='http://www.go-tsunami.com/',
