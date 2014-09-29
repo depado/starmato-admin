126ec69e7f8d9cd7f8fd4d3e8f93b369e3e57ed8
matm <mathias@monnerville.com>
2014-09-29 09:57:16 +0200

    Add missing templates directory

--- setup.py
+++ setup.py
@@ -6,12 +6,22 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+print find_packages()
+
 setup(
     name='starmato-admin',
     version='0.1',
     packages=find_packages(),
     package_data={
-        'starmato.admin': ['media/admin/js/*', 'media/admin/css/*', 'media/admin/images/*', 'locale/fr_FR/LC_MESSAGES/*'],
+        'starmato.admin': [
+            'templates/admin/*', 
+            'templates/admin/edit_inline/*', 
+            'templates/admin/includes/*', 
+            'media/admin/js/*', 
+            'media/admin/css/*', 
+            'media/admin/images/*', 
+            'locale/fr_FR/LC_MESSAGES/*'
+        ],
     },
     license='MIT',
     description='A Django app to upgrade django admin.',
